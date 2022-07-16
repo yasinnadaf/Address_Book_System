f5f9e1508f3bb9fe83269a1c90b7a2fc9d6713f6
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-16 23:38:10 +0530

    Welcome message added

---
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
