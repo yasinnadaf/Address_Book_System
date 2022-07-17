5171aaafc328d3cb96f7fe20eb0d4febce742a1d
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-17 22:39:33 +0530

    Added: Create contacts

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -1,7 +1,89 @@
 package com.bridgelabz.addressbooksystem;
 
-public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-    }
-}
+   public class AddressBook {
+       private String firstName;
+       private String lastName;
+       private String address;
+       private String city;
+       private String state;
+       private int zip;
+       private long phoneNumber;
+       private String email;
+
+       public String getFirstName() {
+           return firstName;
+       }
+
+       public void setFirstName(String firstName) {
+           this.firstName = firstName;
+       }
+
+       public String getLastName(){
+           return lastName;
+       }
+
+       public void setLastName(String lastName) {
+           this.lastName = lastName;
+       }
+
+       public String getAddress(){
+           return address;
+       }
+
+       public void setAddress(String address) {
+           this.address = address;
+       }
+
+       public String getCity(){
+           return city;
+       }
+
+       public void setCity(String city) {
+           this.city = city;
+       }
+
+       public String getState(){
+           return state;
+       }
+
+       public void setState(String state) {
+           this.state = state;
+       }
+
+       public int getzip(){
+           return zip;
+       }
+
+       public void setZip(int zip) {
+           this.zip = zip;
+       }
+
+       public long getPhoneNumber() {
+           return phoneNumber;
+       }
+
+       public void setPhoneNumber(long phoneNumber) {
+           this.phoneNumber = phoneNumber;
+       }
+
+       public String getEmail() {
+           return email;
+       }
+
+       public void setEmail(String email) {
+           this.email = email;
+       }
+        @Override
+       public String toString() {
+            return "Contacts{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", address='" + address + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    ", zip=" + zip +
+                    ", phoneNumber=" + phoneNumber +
+                    ", email='" + email + '\'' +
+                    '}';
+        }
+   }
