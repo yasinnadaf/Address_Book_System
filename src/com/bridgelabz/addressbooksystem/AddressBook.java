2f9e0c6b83ec8fcb6ad095911cbe026c066a2640
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-23 21:34:37 +0530

    Modified multiple contacts

--- src/com/bridgelabz/addressbooksystem/AddressBook.java
+++ src/com/bridgelabz/addressbooksystem/AddressBook.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 public class AddressBook {
     ArrayList<Contact> contactList = new ArrayList<>();
-    Contact contact = new Contact();
+
     Scanner scanner = new Scanner(System.in);
 
-    void addContact(){
+    void addContact() {
+        Contact contact = new Contact();
         System.out.println("Enter first name: ");
         contact.setFirstName(scanner.next());
         System.out.println("Enter last name: ");
@@ -29,54 +30,60 @@ public class AddressBook {
         contactList.add(contact);
     }
 
-    void editContact(){
+    public void showDetails(ArrayList<Contact> contactList) {
+        for(Contact contact : this.contactList){
+            System.out.println(contact.toString());
+        }
+    }
+
+    void editContact() {
 
         System.out.println("enter the first name of a person to edit");
         String firstName = scanner.next();
-        if(firstName.equals(contact.getFirstName())){
-            System.out.println("Edit the details of person: ");
+        for (Contact contact : contactList) {
+            if (firstName.equals(contact.getFirstName())) {
+                System.out.println("Edit the details of person: ");
 
-            System.out.println("Edit first name: ");
-            contact.setFirstName(scanner.next());
+                System.out.println("Edit first name: ");
+                contact.setFirstName(scanner.next());
 
-            System.out.println("Edit last name: ");
-            contact.setLastName(scanner.next());
+                System.out.println("Edit last name: ");
+                contact.setLastName(scanner.next());
 
-            System.out.println("Edit address: ");
-            contact.setAddress(scanner.next());
+                System.out.println("Edit address: ");
+                contact.setAddress(scanner.next());
 
-            System.out.println("Edit city: ");
-            contact.setCity(scanner.next());
+                System.out.println("Edit city: ");
+                contact.setCity(scanner.next());
 
-            System.out.println("Edit state: ");
-            contact.setState(scanner.next());
+                System.out.println("Edit state: ");
+                contact.setState(scanner.next());
 
-            System.out.println("Edit zip code: ");
-            contact.setZip(scanner.nextInt());
+                System.out.println("Edit zip code: ");
+                contact.setZip(scanner.nextInt());
 
-            System.out.println("Edit phone number:");
-            contact.setPhoneNumber(scanner.nextInt());
+                System.out.println("Edit phone number:");
+                contact.setPhoneNumber(scanner.nextInt());
 
-            System.out.println("Edit email: ");
-            contact.setEmail(scanner.next());
+                System.out.println("Edit email: ");
+                contact.setEmail(scanner.next());
+            } else {
+                System.out.println("person not found");
+            }
         }
-
-        else{
-            System.out.println("person not found");
-        }
-
 
     }
 
-    void deleteContact(){
+    void deleteContact() {
         System.out.println("Enter the first name of contact you want to delete");
         String firstName = scanner.next();
-        if(firstName.equals(contact.getFirstName())){
-            contactList.remove(contact);
-            System.out.println("contact deleted successfully");
-        }
-        else{
-            System.out.println("contact not found");
+        for (Contact contact : contactList) {
+            if (firstName.equals(contact.getFirstName())) {
+                contactList.remove(contact);
+                System.out.println("contact deleted successfully");
+            } else {
+                System.out.println("contact not found");
+            }
         }
     }
 }
--- src/com/bridgelabz/addressbooksystem/AddressBookMainClass.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookMainClass.java
@@ -1,14 +1,44 @@
 package com.bridgelabz.addressbooksystem;
 
-public class AddressBookMainClass {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Scanner;
 
-        System.out.println("Welcome to Address Book Program");
-       AddressBook add = new AddressBook();
-       add.addContact();
-        System.out.println(add.contactList);
-        add.editContact();
-        add.deleteContact();
+public class AddressBookMainClass {
+    static final String FAMILY_ADDRESSBOOK = "family address book";
+    static final String PROFESSIONAL_ADDRESSBOOK = "Professional address book";
+    public static void main(String[] args) {
+        HashMap <String,AddressBook> addressBookMap = new HashMap<>();
+
+        AddressBook familyAddressBook = new AddressBook();
+        AddressBook professionalAddressBook = new AddressBook();
+
+        Scanner scr = new Scanner(System.in);
+        boolean exit = true;
+        do {
+            System.out.println("Enter options:\n1) To add contact\n2) To edit Contact\n3) To display Contacts\n4) To delete contact\n5) To exit");
+            int option = scr.nextInt();
+            switch (option) {
+                case 1:
+                    professionalAddressBook.addContact();
+                    break;
+                case 2:
+                    professionalAddressBook.editContact();
+                    break;
+                case 3:
+                    professionalAddressBook.showDetails(professionalAddressBook.contactList);
+                    break;
+                case 4:
+                    professionalAddressBook.deleteContact();
+                    break;
+                case 5:
+                    exit = false;
+                default:
+                    break;
+            }
+        }while(exit);
+
+        addressBookMap.put(FAMILY_ADDRESSBOOK, familyAddressBook);
+        addressBookMap.put(PROFESSIONAL_ADDRESSBOOK, professionalAddressBook);
 
     }
 }
